ccfa393abd4a5b3933ae71afd5263186f8f8ae1c
becario <becario@amei.local>
2019-04-05 10:19:48 +0200

    web-scrapper

--- webscrapper-iptables.py
+++ webscrapper-iptables.py
@@ -1,11 +1,14 @@
-
 import requests                     
 from bs4 import BeautifulSoup      
 import mysql.connector
 import re
+import subprocess
+import base64
+import os
+
 
 q = 0
-p = open("iptables.rules", "w", encoding="utf-8")
+p = open("iptables.rules.out", "w", encoding="utf-8")
 def funcion_datos():
     url = "https://ransomwaretracker.abuse.ch/tracker/"                 
     response = requests.get(url)                                       
@@ -20,9 +23,9 @@ def funcion_datos():
         for i in td:
             if j == 5:
                 if len(i.text) > 8:
-                    ip=(+re.sub(r"([^)]+).*",r"\1)", i.text))
-                    ip=(+re.sub(r"^(+.*",r"\1", ip))
-                    p.write(str(ip)+'\n')
+                    ip = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', i.text).group()
+                    #print(ip)
+                    p.write(str("iptables -I OUTPUT -s "+ ip +" -i DROP"+'\n'))
             j += 1
     h = h - 1
     return
@@ -40,26 +43,14 @@ def funcion_datos_paginas(q):
         j = 0
         for i in td:
             if j == 5:
-                if len(i.text) < 8:
-                    p.write(str("(n/a)")+'\n')
-                    p.write('\n')
-                else:
-                    ip=("IP(Pais)-> "+re.sub(r"([^)]+).*",r"\1)", i.text))
-                    p.write(str(ip)+'\n')
-                    p.write('\n')
-            elif j == 2:
-                p.write(str("Malware-> "+ i.text)+'\n')
-            elif j == 3:
-                p.write(str("Host-> "+ i.text)+'\n')
-            elif j == 4:
-                if len(i.text) < 4:
-                    p.write(str("(n/a)")+'\n')
-                else:
-                    p.write(str("Dominio-> "+ i.text)+'\n')
+                if len(i.text) > 8:
+                    ip = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', i.text).group()
+                    #print(ip)
+                    p.write(str("iptables -I OUTPUT -s "+ ip +" -i DROP"+'\n'))
             j += 1
     h = h - 1
     q = int(q)
-    print(url)
+    #print(url)
     return q
 
 
@@ -72,3 +63,14 @@ for q in range(138+1):
         q += 1
     elif q == 139:
         break
+
+with open('iptables.rules.out', 'rb') as f:
+    data = f.read()
+with open('iptables.rules', 'wb') as f:
+    f.write(base64.encodebytes(data))
+
+#subprocess.call("./home/iptables.sh", shell=True)
+p.close()
+f.close()
+os.remove("iptables.rules.out")
+
